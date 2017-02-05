677dd0e364f8dd65b3bc285df0a112ba5228c8e7
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-02-05 18:07:18 +0100

    Added search filtering

--- app/components/ProductsList.js
+++ app/components/ProductsList.js
@@ -2,11 +2,22 @@ class ProductsList {
     constructor() {
         this.template = `
                 <div class="container-fluid">
-                        <product-display-form ng-repeat="product in $ctrl.products.list" product="product"></product-display-form>
+                    <form>
+                      <div class="form-group">
+                        <input class="form-control" id="searchInput" placeholder="Search" ng-model="$ctrl.query" autocomplete="off">
+                      </div>
+                      <button type="submit" class="btn btn-default" ng-click="$ctrl.search()">Szukaj</button>
+                    </form>
+                
+                    <product-display-form ng-repeat="product in $ctrl.products.list" product="product"></product-display-form>
                 </div>
             `,
             this.controller = function (productsService) {
+                this.query = "";
                 this.products = productsService.get();
+                this.search = () => {
+                    productsService.search(this.query);
+                }
             }
     }
 }
--- app/services/productsService.js
+++ app/services/productsService.js
@@ -5,7 +5,6 @@
         // Przekazanie przez referencję!! Zamiast przez wartość!
         let products = {list: []};
 
-
         $http.get('http://localhost:2095/products').then(function (response) {
             products.list = response.data;
         });
@@ -13,6 +12,17 @@
         return {
             get() {
                 return products;
+            },
+            search(query) {
+                if(query == "") {
+                    $http.get('http://localhost:2095/products').then(function (response) {
+                        products.list = response.data;
+                    });
+                } else {
+                    products.list = products.list.filter(function (value) {
+                        return value.name.startsWith(query);
+                    });
+                }
             }
         }
     }

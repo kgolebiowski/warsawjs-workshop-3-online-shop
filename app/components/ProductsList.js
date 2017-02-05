67cbe4d7df9cef7df85446a2581dc405c635319f
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-02-05 16:36:51 +0100

    Using nodejs mock data service instead of hardcoded data

--- app/components/ProductsList.js
+++ app/components/ProductsList.js
@@ -2,30 +2,11 @@ class ProductsList {
     constructor() {
         this.template = `
                 <div class="container-fluid">
-                        <product-display-form ng-repeat="product in $ctrl.products" product="product"></product-display-form>
+                        <product-display-form ng-repeat="product in $ctrl.products.list" product="product"></product-display-form>
                 </div>
             `,
-            this.controller = function () {
-                this.products = [
-                    {
-                        "id": "26462736-8ea7-5284-8629-257d21c76dbe",
-                        "name": "Chips",
-                        "description": "Bulobsi ure puzu aja gu zoseba zug hochen cibucev ragjozbug nuhosgef pah mul vavcem bucdodol dilef anwoj.",
-                        "image": "http://api.adorable.io/avatars/100/pojko.png"
-                    },
-                    {
-                        "id": "0bc272a4-08a9-58f9-b921-5574f247cea3",
-                        "name": "Tuna",
-                        "description": "Tih cefutji cogtem wikihi zum sijuco jiwji hekmasi wahgid uwsitu nabuec barujgad neguci bofuhmop tar ibi.",
-                        "image": "http://api.adorable.io/avatars/100/pojko.jpg"
-                    },
-                    {
-                        "id": "26462736-8ea7-5284-8629-257d21c76dbe",
-                        "name": "Chips",
-                        "description": "Bulobsi ure puzu aja gu zoseba zug hochen cibucev ragjozbug nuhosgef pah mul vavcem bucdodol dilef anwoj.",
-                        "image": "http://api.adorable.io/avatars/100/pojko.png"
-                    }
-                ]
+            this.controller = function (productsService) {
+                this.products = productsService.get();
             }
     }
 }
---
+++ app/services/productsService.js
@@ -0,0 +1,21 @@
+(function() {
+    "use strict";
+
+    function productsService($http) {
+        // Przekazanie przez referencję!! Zamiast przez wartość!
+        let products = {list: []};
+
+        $http.get('http://localhost:2095/products').then(function (response) {
+            products.list = response.data;
+        });
+
+        return {
+            get() {
+                return products;
+            }
+        }
+    }
+
+    angular.module('shop')
+        .factory('productsService', productsService)
+})();

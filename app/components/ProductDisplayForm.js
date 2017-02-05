c210d0031cea108c2f759d30a7007e05922778fd
Krzysztof Gołębiowski <krzysiek.golebiowski@gmail.com>
2017-02-05 18:23:42 +0100

    Fixed 404 error when page is loaded

--- app/components/ProductDisplayForm.js
+++ app/components/ProductDisplayForm.js
@@ -2,7 +2,7 @@ class ProductDisplayForm {
     constructor() {
         this.template = `<div class="product-item gray-box">
                             <h4>{{$ctrl.product.name}}</h4>
-                            <img src="{{$ctrl.product.image}}" />
+                            <img ng-src="{{$ctrl.product.image}}" />
                             <p>{{$ctrl.product.description}}</p>
                             <button type="submit" class="btn btn-default add-to-cart-button" ng-click="$ctrl.addProduct()">Dodaj do koszyka</button>
                        </div>`;
